ca9a91e32ce7fd92b85cf9b3b94db21de585ac24
Bunny Wong <bunnnywong@gmail.com>
2020-05-01 16:08:03 +0800

    Initial Commit

---
+++ wejh-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wejh-ios
+//
+//  Created by Bunny Wong on 2020/5/1.
+//  Copyright © 2020 Bunny Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
